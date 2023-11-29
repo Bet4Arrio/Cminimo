6e23eeabed3260f8562fbab489ea6fec61a9b82f
Bet4Arrio <emanuelsa-rv@hotmail.com>
2023-11-29 00:06:41 -0300

    Feat: stack implementation

---
+++ lib/stacks.c
@@ -0,0 +1,64 @@
+typedef struct stack {
+	 size_t size;
+	 size_t index;
+	 void* entrys;
+} stack;
+
+#define GROW_FACTOR 4
+
+stack* st_create(void){
+    stack * st = malloc(sizeof(stack));
+    if(st == NULL){
+        printf("Not enough memory");
+        exit(0);
+    }
+    st->size = GROW_FACTOR;
+    st->index = 0;
+    st->entrys = malloc(sizeof*(void*) * st->size);
+    return st
+}
+
+
+stack* st growStack(stack* st){
+    st->size *= GROW_FACTOR;
+    st->entrys = realloc(sizeof*(void*) * st->size);
+
+    return st;
+}
+void* st_pop(stack* st){
+    if (st->index == 0)
+        return st->entrys[st->index];
+    st->index--;
+    return st->entrys[st->index+1]
+}
+
+void* st_push(stack* st, void* value){
+    if(st->index + 1 >= st->size){
+        growStack(st);
+    }
+
+    st->index++;
+    void* cpValue = malloc(sizeof(value));
+    memcpy(cpValue, value, sizeof(value));
+    st->entrys[st->index] = cpValue;
+    return cpValue;
+}
+
+int st_clean(stack* st){
+    for (size_t i = 0; i < st->size; i++){
+        free(st->entrys[i]);
+    }
+
+    st->index = 0;
+
+    return 1;
+    
+}
+
+int st_destroy(stack* st){
+    for (size_t i = 0; i < st->size; i++){
+        free(st->entrys[i]);
+    }
+    free(st);
+    return 1;
+}
--- lib/stacks.h
+++ lib/stacks.h
@@ -1,2 +1,15 @@
+#include<stdlib.h>
+#include <string.h>
+#include <stdint.h>
 
 typedef struct stack stack;
+
+stack* st_create(void);
+
+void* st_pop(stack*);
+
+void* st_push(stack*, void*);
+
+int st_clean(stack*);
+
+int st_destroy(stack*);
